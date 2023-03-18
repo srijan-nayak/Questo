d3410a675f7f4e5a5d80c8f310154bd9e1f053aa
Srijan Nayak <srijannayak0@gmail.com>
2023-03-18 20:35:57 +0530

    Add Sanity vision tool for dev environment

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,17 +1,18 @@
 import getConfig from "next/config";
-import { defineConfig } from "sanity";
+import { defineConfig, isDev } from "sanity";
 import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
 
 import { schemaTypes } from "@/schemas";
 
-const {publicRuntimeConfig} = getConfig();
+const { publicRuntimeConfig } = getConfig();
 
 export default defineConfig({
   basePath: "/admin",
   title: "Questo",
   projectId: publicRuntimeConfig.SANITY_PROJECT_ID!,
   dataset: publicRuntimeConfig.SANITY_DATASET!,
-  plugins: [deskTool()],
+  plugins: [deskTool(), ...(isDev ? [visionTool()] : [])],
   schema: {
     types: schemaTypes,
   },
