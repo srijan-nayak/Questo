cbe41edb8d39ddfcc62186fe7c96313d173b5ed6
Srijan Nayak <srijannayak0@gmail.com>
2023-03-20 17:40:26 +0530

    Create sanity client

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
     SANITY_DATASET: process.env.SANITY_DATASET,
   },
+  serverRuntimeConfig: {
+    SANITY_TOKEN: process.env.SANITY_TOKEN,
+  },
 };
 
 module.exports = nextConfig;
