8b7e8c7faa09f147bb2ceb2900054f192b6f23b4
Srijan Nayak <srijannayak0@gmail.com>
2023-03-24 15:35:32 +0530

    Add nav bar to the layout component

---
+++ components/nav-bar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between py-4">
+      <Link href="/" className="font-bold text-emerald-700">
+        Questo
+      </Link>
+
+      <ul className="flex gap-6 text-sm font-semibold">
+        <li>
+          <Link
+            href="/questions/new"
+            className="bg-emerald-600 hover:bg-emerald-500 text-gray-50 py-1 px-3 rounded"
+          >
+            Ask
+          </Link>
+        </li>
+        <li>
+          <Link href="/questions" className="text-gray-600 hover:underline">
+            All Questions
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/layout";
 const Home = () => {
   return (
     <Layout>
-      <h1 className="text-5xl font-thin">Questo</h1>
+      <h1 className="text-3xl font-bold text-gray-500">Questo</h1>
     </Layout>
   );
 };
