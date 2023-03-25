508b626672021ddadab80ce748d11dc6c3df68c4
Srijan Nayak <srijannayak0@gmail.com>
2023-03-25 11:51:32 +0530

    Fetch questions data for /questions page

---
+++ pages/questions/index.tsx
@@ -0,0 +1,33 @@
+import { GetServerSideProps } from "next";
+import { Question } from "@/schemas/schema";
+import client from "@/sanity-clients/client";
+import Layout from "@/components/layout";
+
+interface QuestionsPageProp {
+  questions: Question[];
+}
+
+const QuestionsPage = ({ questions }: QuestionsPageProp) => {
+  return (
+    <Layout>
+      {/*TODO: Build questions page UI according to design*/}
+      {questions.map((question) => (
+        <p key={question._id}>{question.title}</p>
+      ))}
+    </Layout>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const questions: Question[] = await client.fetch(
+    `*[_type == "question"] | order(_createdAt desc)`
+  );
+
+  return {
+    props: {
+      questions,
+    },
+  };
+};
+
+export default QuestionsPage;
