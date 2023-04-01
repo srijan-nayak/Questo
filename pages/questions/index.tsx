fe9dd36a75e6eba3ebd7ff9e78b11ccf382aa7d1
Sindhu <sindhu.pl@outlook.com>
2023-04-02 00:13:55 +0530

    Build Questions Page UI

--- pages/questions/index.tsx
+++ pages/questions/index.tsx
@@ -12,9 +12,22 @@ const QuestionsPage = ({ questions }: QuestionsPageProp) => {
   return (
     <Layout>
       {/*TODO: Build questions page UI according to design*/}
-      {questions.map((question) => (
-        <Link href={`/questions/${question._id}`} key={question._id}>{question.title}</Link>
-      ))}
+      <div className="flex flex-col justify-items-center gap-10">
+        <h1 className="text-3xl leading-9 font-bold text-gray-500">
+          Questions
+        </h1>
+        <ul className="flex flex-col gap-8">
+          {questions.map((question) => (
+            <Link
+              className="text-xl leading-7 font-semibold gap-2 px-4 py-3 rounded drop-shadow-lg bg-gray-50 text-emerald-600"
+              href={`/questions/${question._id}`}
+              key={question._id}
+            >
+              {question.title}
+            </Link>
+          ))}
+        </ul>
+      </div>
     </Layout>
   );
 };
