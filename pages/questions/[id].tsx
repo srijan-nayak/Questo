f11199c7ca4a71d46f8ca57205f16c5856dbfe60
Sreenivasyadav374 <Srinivaspa374@gmail.com>
2023-04-02 10:41:54 +0530

    Update [id].tsx
    Update UI for the questions page.

--- pages/questions/[id].tsx
+++ pages/questions/[id].tsx
@@ -1,10 +1,19 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import { useRouter } from "next/router";
-import { Answer, Question } from "@/schemas/schema";
+import createClient from "@/sanity-clients/client";
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  GetStaticPropsContext,
+  PreviewData,
+} from "next";
+import { Question } from "@/schemas/schema";
+import { Answer } from "@/schemas/schema";
 import Layout from "@/components/layout";
-import client from "@/sanity-clients/client";
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
 
-interface QuestionDetailsPageProps {
+const client = createClient;
+
+interface prop {
   question: Question[];
   answer: Answer[];
 }
@@ -17,7 +26,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
   return { paths, fallback: true };
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext<ParsedUrlQuery, PreviewData>
+) => {
   const id = context.params!.id;
   let question;
 
@@ -38,40 +49,37 @@ export const getStaticProps: GetStaticProps = async (context) => {
   };
 };
 
-const QuestionDetailsPage = (props: QuestionDetailsPageProps) => {
+const QuestionDetails = (props: prop) => {
   const router = useRouter();
 
   return (
     <Layout>
       {router.isFallback ? (
-        <div className="text-lg text-gray-500">Loading...</div>
+        <p className="text-lg text-gray-500">Loading...</p>
       ) : (
-        <div className="flex-col justify-items-center mx-4 my-76 space-y-10">
-          <div className="text-justify text-gray-500 font-bold max-w-3xl max-h-9 text-3xl">
+        <div className="flex flex-col gap-10">
+          <h1 className="text-gray-500 font-bold text-3xl">
             {props.question[0].title}
-          </div>
-          <div className="flex-col space-y-4 px-5 py-4 rounded drop-shadow-lg bg-gray-50">
-            <div className="font-semibold text-sm text-gray-500">
+          </h1>
+          <section className="flex flex-col gap-4 px-5 py-4 rounded drop-shadow-lg bg-gray-50">
+            <h2 className="text-sm leading-5 font-semibold text-gray-500">
               QUESTION DETAILS
-            </div>
-            <div className="font-semibold text-gray-600 whitespace-pre-line">
+            </h2>
+            <p className="text-base leading-6 font-semibold text-gray-600">
               {props.question[0].details}
-            </div>
-          </div>
-          {props.answer[0]?.text && (
-            <div className="flex-col bg-emerald-600 rounded px-5 py-4 space-y-4 text-gray-50 drop-shadow-lg">
-              <div className="font-semibold text-sm text-emerald-100">
-                ANSWER
-              </div>
-              <div className="font-semibold text-gray-50 whitespace-pre-line">
-                {props.answer[0]?.text}
-              </div>
-            </div>
+            </p>
+          </section>
+          {props.answer[0]?.text ? (
+            <section className="flex flex-col bg-emerald-600 rounded px-5 py-4 gap-4 text-gray-50 drop-shadow-lg">
+              <h2 className="text-sm leading-5 font-semibold">ANSWER</h2>
+              <p>{props.answer[0]?.text}</p>
+            </section>
+          ) : (
+            <p className="px-5 py-4">NOT YET ANSWERED</p>
           )}
         </div>
       )}
     </Layout>
   );
 };
-
-export default QuestionDetailsPage;
+export default QuestionDetails;
