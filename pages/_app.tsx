2d5626f6ec137c00c6c8a93d13aaa6f253b00d96
Srijan Nayak <srijannayak0@gmail.com>
2023-04-08 18:34:55 +0530

    Add progress bar to indicate pages are loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import LoadingBar from "react-top-loading-bar";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => setProgress(40));
+    router.events.on("routeChangeComplete", () => setProgress(100));
+    router.events.on("routeChangeError", () => setProgress(100));
+  });
+
+  return (
+    <>
+      <LoadingBar
+        color="#059669"
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
